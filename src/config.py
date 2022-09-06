cd812db06abd941c7192f0243d4937c20a003ec6
RusLANx86 <62020743+RusLANx86@users.noreply.github.com>
2022-09-06 12:45:05 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import os
 
-token = '25423453465345:AAFL7HMubMKevAsX2TFEnve7VUzAtW707xE'
+token = ''
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-chat_id = -2342345345456546 #id мемландии
+chat_id = ''
